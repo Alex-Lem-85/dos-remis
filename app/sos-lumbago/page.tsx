030db0b3b9c7df797963c18cc3b233f60efbac5b
Alexandre Lemaitre <a.lemaitre85@gmail.com>
2025-01-26 15:07:27 +0100

    Update page.tsx

--- app/sos-lumbago/page.tsx
+++ app/sos-lumbago/page.tsx
@@ -30,12 +30,26 @@ const faqItems = [
   Dans la majorité des cas de lumbago, il n'est pas nécessaire de réaliser une radio, un scanner ou une IRM. Ces examens ne sont recommandés que dans des situations particulières, que votre médecin saura identifier. En cas de présence de 
   <span
     className="text-primary underline cursor-pointer"
-    onClick={() => alert('Drapeaux rouges :\n\n1. Douleur de type non mécanique : douleur d\'aggravation progressive, présente au repos et en particulier durant la nuit\n2. Symptôme neurologique étendu (déficit dans le contrôle des sphincters vésicaux ou anaux, atteinte motrice au niveau des jambes, syndrome de la queue-de-cheval)\n3. Paresthésie au niveau du pubis (ou périnée)\n4. Traumatisme important (tel qu\'une chute de hauteur)\n5. Perte de poids inexpliquée\n6. Antécédent de cancer\n7. Usage de drogue intraveineuse ou usage prolongé de corticoïdes\n8. Déformation structurale importante de la colonne\n9. Douleur thoracique (rachialgies dorsales)\n10. Âge d\'apparition inférieur à 20 ans ou supérieur à 55 ans\n11. Fièvre\n12. Altération de l\'état général')}
+    onClick={() =>
+      window.alert(`Drapeaux rouges :
+1. Douleur de type non mécanique : douleur d'aggravation progressive, présente au repos et en particulier durant la nuit
+2. Symptôme neurologique étendu (déficit dans le contrôle des sphincters vésicaux ou anaux, atteinte motrice au niveau des jambes, syndrome de la queue-de-cheval)
+3. Paresthésie au niveau du pubis (ou périnée)
+4. Traumatisme important (tel qu'une chute de hauteur)
+5. Perte de poids inexpliquée
+6. Antécédent de cancer
+7. Usage de drogue intraveineuse ou usage prolongé de corticoïdes
+8. Déformation structurale importante de la colonne
+9. Douleur thoracique (rachialgies dorsales)
+10. Âge d'apparition inférieur à 20 ans ou supérieur à 55 ans
+11. Fièvre
+12. Altération de l'état général`)}
   >
     drapeaux rouges
   </span>
   , il est préférable d'aller vers un service d'urgence.
 </p>
+
       </>
     )
   },
