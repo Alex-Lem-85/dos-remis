3b25ddaf899460d0b543eceb1a6875943cf83ab0
Alexandre Lemaitre <a.lemaitre85@gmail.com>
2025-01-26 15:57:59 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,35 @@ import { Footer } from '@/components/footer';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Dos-remis | Spécialistes du lumbago',
-  description: 'Centre spécialisé dans le traitement de la lombalgie aiguë par l\'équipe Dos-remis',
+  title: 'Dos-remis | Spécialistes en lumbago à Sète',
+  description: 'Vous souffrez de lombalgie aiguë, de mal au dos ou de dos bloqué ? Le centre SOS Lumbago à Sète est spécialisé dans le traitement rapide et efficace des douleurs dorsales grâce à des infiltrations et des soins personnalisés.',
+  keywords: [
+    'lombalgie aiguë', 
+    'lombalgie', 
+    'mal au dos', 
+    'SOS lumbago', 
+    'dos bloqué', 
+    'Sète', 
+    'dos', 
+    'infiltration', 
+    'traitement', 
+    'douleur dorsale', 
+    'spécialistes du dos', 
+    'soins du dos'
+  ],
+  openGraph: {
+    title: 'Dos-remis | Spécialistes en lumbago à Sète',
+    description: 'Découvrez Dos-remis à Sète : une équipe spécialisée pour soulager vos douleurs dorsales, traiter la lombalgie aiguë, et retrouver rapidement votre mobilité grâce à des techniques modernes comme SOS lumbago.',
+    url: 'https://dos-remis.com/',
+    images: [
+      {
+        url: 'https://votre-site.com/images/lombalgie-dos.jpg',
+        alt: 'Traitement lombalgie aiguë - SOS Lumbago',
+      },
+    ],
+  },
 };
+
 
 export default function RootLayout({
   children,
