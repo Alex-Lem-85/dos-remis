0df180a94c8bafa5b8b736e0aced745894889289
Alexandre Lemaitre <a.lemaitre85@gmail.com>
2025-01-28 12:09:14 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,16 +56,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" suppressHydrationWarning>
-      <head>
-        {/* Favicon configuration for all platforms */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="background-color" content="#ffffff" />
-      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
