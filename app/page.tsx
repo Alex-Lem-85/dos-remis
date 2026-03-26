e8645c8cae32c488425c84f5c7322bfcdeffc5f3
Alexandre Lemaitre <alex@inkosi.fr>
2026-03-26 18:53:40 +0100

    Add background color to features section

--- app/page.tsx
+++ app/page.tsx
@@ -152,7 +152,7 @@ export default function Home() {
       </section>
       
       {/* Features */}
-      <section className="py-16">
+      <section className="py-16 bg-gray-50">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
             <Card>
