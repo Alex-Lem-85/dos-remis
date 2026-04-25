d4e702690350ea00c740e7f71f4067476aee07ca
Alexandre Lemaitre <alex@inkosi.fr>
2026-04-25 10:11:29 +0200

    Enhance event description for Dr. Ranoux
    Updated the description for the event with Dr. Ranoux to include more details about the discussion and his book.

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ const newsItems = [
     {
     title: "Rencontre autour des fascias avec le Dr Ranoux",
     description:
-      "Nous aurons la chance d’accueillir le Dr Ranoux lors de notre soirée du 19 juin des échanges passionnants autour des fascias",
+      "Nous aurons la chance d’accueillir le Dr Ranoux, auteur du livre "Fascias - Les connaître pour mieux les soigner" lors de notre soirée du 19 juin. Cela promet des échanges passionnants autour des fascias (nous rélisons notre infiltration dans le fascia thoraco-lombaire)",
     video: "https://www.youtube.com/embed/dCzdCsyUpTk",
   },
 ];
